99d29ed5f57873df1e444329b2a7f17caabc7562
Michael <30883926+mgalde@users.noreply.github.com>
2018-09-25 21:09:38 -0500

    uppy

--- api/urls.py
+++ api/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     url(r'^session', csrf_exempt(controllers.Session.as_view())),
     url(r'^register', csrf_exempt(controllers.Register.as_view())),
     url(r'^events', csrf_exempt(controllers.Events.as_view())),
+    url(r'^dog', csrf_exempt(controllers.Events.as_view())),
     url(r'^activateifttt', csrf_exempt(controllers.ActivateIFTTT.as_view())),
     url(r'^', include(router.urls)),
 ]
